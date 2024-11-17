66cd909897ac812ddf265224160d7e4268501feb
木村光弘 <m.kimura@futureantiques.co.jp>
2024-11-17 12:04:01 +0900

    test

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
-  console.log(JSON.stringify(process.env, null, 2))
+  console.log("process.env:"+ JSON.stringify(process.env, null, 2))
+  console.log("secrets:"+ JSON.stringify(secrets, null, 2))
+
 } catch (error) {
   core.setFailed(error.message);
 }
